d614e2317290c3d67ce1371a136cd32a18f36105
Shailla Maria da Silva Maia <112740621+ShayllaMaia@users.noreply.github.com>
2023-06-16 21:36:40 -0300

    Update FiltroCors.java

--- sgcmapi/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ sgcmapi/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -17,7 +17,8 @@ public class FiltroCors {
         corsConfig.setAllowCredentials(true);
         corsConfig.setAllowedOrigins(Arrays.asList(
             "http://127.0.0.1:5500",
-            "https://localhost:4200"
+            "https://localhost:4200",
+            "https://integracao-continua-5neiwiz65-shayllamaia.vercel.app/"
         ));
         corsConfig.setAllowedMethods(Arrays.asList("*"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
